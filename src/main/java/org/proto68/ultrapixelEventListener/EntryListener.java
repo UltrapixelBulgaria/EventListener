ad686f3d3a1b0be8f5b32c61669c47a9e8d4d0da
Proto68 <timkaq67@gmail.com>
2026-06-24 22:17:34 +0300

    Fixed player saving and updated the dependency

--- src/main/java/org/proto68/ultrapixelEventListener/EntryListener.java
+++ src/main/java/org/proto68/ultrapixelEventListener/EntryListener.java
@@ -1,14 +1,15 @@
 package org.proto68.ultrapixelEventListener;
 
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Set;
-import net.raidstone.wgevents.events.RegionEnteredEvent;
+import org.proto68.wgevents.events.RegionEnteredEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
 
 public class EntryListener implements Listener {
     private final EventListener plugin;
@@ -17,19 +18,23 @@ public class EntryListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(
-            priority = EventPriority.LOWEST
-    )
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onRegionEntered(RegionEnteredEvent event) {
         Player player = Bukkit.getPlayer(event.getUUID());
         if (player != null) {
             String regionName = event.getRegionName();
-            Set<String> regionNames = Objects.requireNonNull(this.plugin.getDataConfig().getConfigurationSection("regions")).getKeys(false);
-            if (regionNames.contains(regionName)) {
-                this.plugin.getTempConfig().set("players." + regionName + "." + player.getName(), LocalDateTime.now().toString());
-                this.plugin.saveDataFile();
-            }
+            Set<String> regionNames = Objects
+                    .requireNonNull(this.plugin.getDataConfig().getConfigurationSection("regions"))
+                    .getKeys(false);
 
+            if (regionNames.contains(regionName)) {
+                String path = "players." + regionName + "." + player.getName();
+
+                if (!this.plugin.getTempConfig().contains(path)) {
+                    this.plugin.getTempConfig().set(path, LocalDateTime.now().toString());
+                    this.plugin.saveDataFile();
+                }
+            }
         }
     }
 }
